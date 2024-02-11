870472193effe073a4d856e23c8e9b04157e2f29
cloud-network-guy <greenlakejohnny@gmail.com>
2024-02-11 14:13:13 -0800

    network viewer feature

--- shared_vpc.tf
+++ shared_vpc.tf
@@ -20,8 +20,6 @@ locals {
   }
   # Create a list of objects for all subnets that are shared
   shared_subnets = flatten([for k, v in local.subnets :
-    concat(
-      length(v.attached_projects) > 0 || length(v.shared_accounts) > 0 && !v.is_proxy_only ? [
         {
           subnet_key = "${v.index_key}-user"
           project_id = v.project_id
@@ -31,22 +29,22 @@ locals {
           members = toset(flatten(concat([
             for i, service_project_id in v.attached_projects : lookup(local.compute_sa_accounts, service_project_id, [])
           ], v.shared_accounts)))
-      }] : [],
-      length(v.viewer_accounts) > 0 && !v.is_proxy_only ? [
+      } if length(v.attached_projects) > 0 || length(v.shared_accounts) > 0 && !v.is_proxy_only
+  ])
+  # Same for viewer
+  viewable_subnets = flatten([for k, v in local.subnets :
         {
-          subnet_key = "${v.index_key}-viewer"
+          subnet_key = v.index_key
           project_id = v.project_id
           region     = v.region
           subnetwork = "projects/${v.project_id}/regions/${v.region}/subnetworks/${v.name}"
           role       = "roles/compute.networkViewer"
           members    = toset(v.viewer_accounts)
-        }
-      ] : []
-    )
+        } if       length(v.viewer_accounts) > 0 && !v.is_proxy_only
   ])
 }
 
-# Give Compute Network User & Viewer permissions on the subnet to the applicable accounts
+# Give Compute Network User permissions on the subnet to the applicable accounts
 resource "google_compute_subnetwork_iam_binding" "default" {
   for_each   = { for i, v in local.shared_subnets : v.subnet_key => v }
   project    = each.value.project_id
@@ -57,3 +55,13 @@ resource "google_compute_subnetwork_iam_binding" "default" {
   depends_on = [google_compute_subnetwork.default]
 }
 
+# Give Compute Network Viewer permissions on the subnet to the applicable accounts
+resource "google_compute_subnetwork_iam_binding" "viewer" {
+  for_each   = { for i, v in local.viewable_subnets : v.subnet_key => v }
+  project    = each.value.project_id
+  region     = each.value.region
+  subnetwork = each.value.subnetwork
+  role       = each.value.role
+  members    = each.value.members
+  depends_on = [google_compute_subnetwork.default]
+}
